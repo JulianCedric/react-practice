4fb335e2db2c96d1183970db61f7f71a8b5d4e44
JulianCedric <julian.pormentilla@gmail.com>
2020-11-21 00:34:35 -0500

    Updates to Home Page

--- src/App.js
+++ src/App.js
@@ -4,134 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './Navbar';
 import Home from './Home';
 
-QUOTES = [
-  {
-  "id": 4,
-  "user_id": 1,
-  "name": "",
-  "description": "Write 1 new thing I learned today in my journal before bedtime",
-  "quote": "'Practice isn't the thing you do once you're good. It's the thing you do that makes you good.' -Malcolm Gladwell",
-  "dateCreated": null,
-  "column": "A",
-  "counter": 0,
-  "percentage": 0,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.054Z",
-  "updated_at": "2020-09-28T06:07:03.054Z"
-  },
-  {
-  "id": 5,
-  "user_id": 1,
-  "name": "",
-  "description": "Practice piano for at least 20 minutes",
-  "quote": "'Successful people are simply those with successful habits.' -Brian Tracy",
-  "dateCreated": null,
-  "column": "A",
-  "counter": 0,
-  "percentage": 0,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.075Z",
-  "updated_at": "2020-09-28T06:07:03.075Z"
-  },
-  {
-  "id": 6,
-  "user_id": 1,
-  "name": "",
-  "description": "Read at least 3 pages of a fiction novel",
-  "quote": "'The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.' -Albert Einstein",
-  "dateCreated": null,
-  "column": "A",
-  "counter": 0,
-  "percentage": 0,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.097Z",
-  "updated_at": "2020-09-28T06:07:03.097Z"
-  },
-  {
-  "id": 7,
-  "user_id": 1,
-  "name": "",
-  "description": "Water plants in my room!",
-  "quote": "'Drop by drop is the water pot filled.' -BUDDHA",
-  "dateCreated": null,
-  "column": "A",
-  "counter": 20,
-  "percentage": 90,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.106Z",
-  "updated_at": "2020-09-28T06:07:03.106Z"
-  },
-  {
-  "id": 8,
-  "user_id": 1,
-  "name": "",
-  "description": "Drink 1 more glass of water today",
-  "quote": "'I fear not the man who has practiced 10,000 kicks, but I do fear the man who has practiced one kick 10,000 times.' -BRUCE LEE",
-  "dateCreated": null,
-  "column": "A",
-  "counter": 7,
-  "percentage": 33,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.113Z",
-  "updated_at": "2020-09-28T06:07:03.113Z"
-  },
-  {
-  "id": 9,
-  "user_id": 1,
-  "name": "",
-  "description": "Make bed before morning jog",
-  "quote": "'The chains of habit are too weak to be felt until they are too strong to be broken.'   --Samuel Johnson",
-  "dateCreated": null,
-  "column": "C",
-  "counter": 21,
-  "percentage": 100,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.125Z",
-  "updated_at": "2020-09-28T06:07:03.125Z"
-  },
-  {
-  "id": 10,
-  "user_id": 1,
-  "name": "",
-  "description": "Prep gym clothes next to bed the night before a work-out day",
-  "quote": "'Let today be the day you give up who you've been for who you can become.' -Hal Elrod",
-  "dateCreated": null,
-  "column": "C",
-  "counter": 21,
-  "percentage": 100,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.132Z",
-  "updated_at": "2020-09-28T06:07:03.132Z"
-  },
-  {
-  "id": 11,
-  "user_id": 1,
-  "name": "",
-  "description": "Study / Practice Algorithms & Data Structures for at least an hour",
-  "quote": "'It's not what we do once in a while that shapes our lives, but what we do consistently.' -Tony Robbins",
-  "dateCreated": null,
-  "column": "C",
-  "counter": 21,
-  "percentage": 100,
-  "dailyWinConfirmation": false,
-  "created_at": "2020-09-28T06:07:03.139Z",
-  "updated_at": "2020-09-28T06:07:03.139Z"
-  }
-  ]
-
 class App extends React.Component {
-state = {
-  quotes: []
-}
-
-componentDidMount = () => {
-  fetch(QUOTES)
-  .then(r => r.json())
-  .then(usersData => this.setState({ users: usersData }))
-
-
+  render() {
   return (
-    <Router>
+    <Router> 
       <React.Fragment>
         <Switch>
           <div className="App">
@@ -142,7 +18,8 @@ componentDidMount = () => {
         </Switch>
       </React.Fragment>
     </Router>
-  )
+    )
+  }
 }
 
 export default App;
---
+++ src/quotesContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class quotesContainer extends React.Component {
+    render() { 
+
+        return (  
+            <div>
+                <h1>
+                    quotesContainer     
+                </h1> 
+            </div>
+        );
+    }
+}
+ 
+export default quotesContainer;
